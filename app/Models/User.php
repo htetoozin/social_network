c5ac37f78b963e1343bc7e54b74e699c07d5d976
htetoozin <htetoo.zin09@gmail.com>
2017-09-11 23:15:32 +0630

    Showing Friends

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,21 @@ class User extends Authenticatable
     {
         return "https://www.gravatar.com/avatar/{{ md5($this->email) }}?d=mm&s=40";    
     }
+
+    //Customize Relationships
+    public function friendsOfMine()
+    {
+        return $this->belongsToMany('App\Models\User', 'friends', 'user_id', 'friend_id');    
+    }
+
+    public function friendOf()
+    {
+        return $this->belongsToMany('App\Models\User', 'friends', 'friend_id', 'user_id');    
+    }
+
+
+    public function friends()
+    {
+        return $this->friendsOfMine()->wherePivot('accepted', true)->get()->merge($this->friendOf()->wherePivot('accepted', true)->get());    
+    }
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -9,7 +9,16 @@
 		</div>
 
 		<div class="col-lg-4 col-lg-offset-3">
-			
+			<h4>{{ $user->getNameOrUsername() }}'s friends</h4>
+
+			@if(!$user->friends()->count())
+				<p>{{ $user->getNameOrUsername() }}'s no friends</p>
+
+			@else
+				@foreach($user->friends() as $user)
+					@include('users.partials.userblock')
+				@endforeach
+			@endif
 		</div>
 	</div>
 
